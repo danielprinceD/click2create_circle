f41799bd19c3aa47c74a575ee318fdb51e239817
daniel <d.danielprince2003@gmail.com>
2023-10-15 13:32:06 +0530

    create sphere on 3d plane by clicking

--- main.js
+++ main.js
@@ -22,21 +22,55 @@ const axes = new THREE.AxesHelper(5)
 scene.add(axes)
 
   const orbit = new OrbitControls(camera,renderer.domElement)
-const ballGeo = new THREE.SphereGeometry()
-const ballMat = new THREE.MeshBasicMaterial(
-  {color:'0x00ff00'}
-)
-const ball = new THREE.Mesh(ballGeo,ballMat)
-scene.add(ball)
+// const ballGeo = new THREE.SphereGeometry(0.5)
+// const ballMat = new THREE.MeshBasicMaterial(
+//   {color:'0x00ff00'}
+// )
+// const ball = new THREE.Mesh(ballGeo,ballMat)
+// scene.add(ball)
 camera.position.set(0,0,10)
 camera.position.z=10
 orbit.update()
 
+
+const mouse = new THREE.Vector2()
+const planeNormal = new THREE.Vector3()
+const intersectPoint = new THREE.Vector3()
+const plane = new THREE.Plane()
+const raycaster = new THREE.Raycaster()
+
+window.addEventListener('mousemove',(e)=>
+{
+  mouse.x= (e.clientX/window.innerWidth)*2-1
+  mouse.y= -(e.clientY/window.innerHeight)*2+1
+  planeNormal.copy(camera.position).normalize()
+  plane.setFromNormalAndCoplanarPoint(planeNormal,scene.position)
+  raycaster.setFromCamera(mouse,camera)
+  raycaster.ray.intersectPlane(plane,intersectPoint)
+})
+
+window.addEventListener('click',(e)=>
+{
+  const sphereGeo = new THREE.SphereGeometry(0.5)
+  const sphereMat = new THREE.MeshBasicMaterial(
+    {
+      color:0xff0f00
+    }
+  )
+  const sphere = new THREE.Mesh(sphereGeo,sphereMat)
+  scene.add(sphere)
+
+  sphere.position.copy(intersectPoint)
+})
+
+
+
+
 function animate ()
 {
-  ball.rotation.x+=0.01/10
+  // ball.rotation.x+=0.01/10
   renderer.render(scene,camera)
-
 }
+
 
 renderer.setAnimationLoop(animate)
